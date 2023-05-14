53c5b66b153099ed666a2cd747b26cc0f8baa684
idikutkasgarli <idikutkasgarli@hotmail.com>
2023-05-14 04:11:42 +0300

    GridSystem
    android support must be opened in built settings
    I used simple 2D assets for the game feel free to change them(it is good though)
    In Grid Object The GridSquare prefab must be placed, also you can change the sprites in that prefab's scripts called "GridSquareScript"

---
+++ Assets/Idikut/Scripts/Grids/GridScript.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour
+{
+    public int columns = 10, rows = 10;
+    public float squareSize = 0.1f;
+    public float squareGap = 0.1f;
+    public GameObject gridSquare;
+    public Vector2 startPosition = new Vector2(0, 0);
+    public float squareScale = 0.5f;
+    public float everySquareScale = 0.0f;
+
+    private Vector2 offset = new Vector2(0, 0);
+    private List<GameObject> gridSquares = new List<GameObject>();
+
+    void Start()
+    {
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        SpawnGridSquares();
+        SetGridSquaresPosition();
+    }
+
+    private void SpawnGridSquares()
+    {
+        int squareIndex = 0;
+        for (int row = 0; row < rows; row++)
+        {
+            for (int column = 0; column < columns; column++)
+            {
+                gridSquares.Add(Instantiate(gridSquare) as GameObject);
+                gridSquares[gridSquares.Count - 1].transform.SetParent(this.transform);
+                gridSquares[gridSquares.Count - 1].transform.localScale = new Vector3(squareScale, squareScale, squareScale);
+                gridSquares[gridSquares.Count - 1].GetComponent<GridSquareScript>().SetImage(squareIndex % 2 == 0);
+                squareIndex++;
+            }
+        }
+    }
+
+    private void SetGridSquaresPosition()
+    {
+        int columnNumber = 0;
+        int rowNumber = 0;
+        bool reverseDirection = false;
+        
+        var squareRect = gridSquares[0].GetComponent<RectTransform>().rect;
+        offset.x = squareRect.width * squareScale + everySquareScale;
+        offset.y = squareRect.height * squareScale + everySquareScale;
+
+        foreach (GameObject square in gridSquares)
+        {
+            if (columnNumber >= columns)
+            {
+                columnNumber = 0;
+                rowNumber++;
+                reverseDirection = !reverseDirection;
+            }
+
+            var xPos = offset.x * columnNumber + (columnNumber * squareGap);
+            var yPos = offset.y * rowNumber + (rowNumber * squareGap);
+
+            if (reverseDirection)
+            {
+                xPos = offset.x * (columns - 1 - columnNumber) + ((columns - 1 - columnNumber) * squareGap);
+            }
+
+            square.GetComponent<RectTransform>().anchoredPosition = new Vector2(startPosition.x + xPos, startPosition.y - yPos);
+            square.GetComponent<RectTransform>().localPosition = new Vector3(startPosition.x + xPos, startPosition.y - yPos, 0f);
+
+            columnNumber++;
+        }
+    }
+}
---
+++ Assets/Idikut/Scripts/Grids/GridSquareScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSquareScript : MonoBehaviour
+{
+    public Image normalImage;
+    public List<Sprite> normalImages;
+    void Start()
+    {
+        
+    }
+
+    public void SetImage(bool SetFirstImage)
+    {
+        if(SetFirstImage)       
+            normalImage.sprite = normalImages[0];
+
+        else
+            normalImage.sprite = normalImages[1];
+    }
+}
