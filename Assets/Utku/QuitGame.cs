24968e0e209e0355c7ff84c68b5c58e3238b899a
utkuturemen <utkuturemen10@gmail.com>
2023-05-15 18:28:10 +0300

    MainMenu(Settings-Music(OnOffButton)

---
+++ Assets/Utku/QuitGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void QuitGameFunction()
+    {
+        Application.Quit();
+        Debug.Log("ÇIKTIK");
+    }
+
+
+}
